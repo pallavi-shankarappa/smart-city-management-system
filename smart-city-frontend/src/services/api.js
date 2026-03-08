31c0b5d80f081602258b2611ad9033940a5a675a
Pallavi S <pallavishankarappa123@gmail.com>
2026-03-08 23:00:58 +0530

    update api url

--- smart-city-frontend/src/services/api.js
+++ smart-city-frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // so the browser never talks to the backend origin directly (no CORS issue).
 
 const API = axios.create({
-  baseURL: "https://smart-city-management-system-1-ww8u.onrender.com",
+  baseURL: "https://smart-city-management-system-1-h0io.onrender.com",
 });
 // Add token automatically if exists
 API.interceptors.request.use((req) => {
